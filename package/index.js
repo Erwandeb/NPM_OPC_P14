1aa9640c355d1f470e4f300a755c1fe1230d8344
Erwan de Belleville <edebelleville@hotmail.fr>
2021-12-06 11:00:58 +0100

    add jquery file and update package

---
+++ package/index.js
@@ -0,0 +1,16 @@
+//import React from "react";
+
+
+export const DropdownList = (props) => {
+    if(!props.options) {
+        return null
+    }
+    return (
+        <select className="dropdown-list" onChange={(e)=>props.onChange(e.target.value)}>
+            {
+            props.options.map(element => 
+            <option key={element.id} value={element.id}>{element.value}</option>)
+            }
+        </select>
+    );
+};
